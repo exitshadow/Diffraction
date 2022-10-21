ebed540819d5eedeee09e4387e01697c1afd2b91
exitshadow <exertingshadows@gmail.com>
2022-10-21 22:11:28 +0200

    first successful implementation of tween on RectTransform

--- Assets/Scripts/HUDs & UI/UIAnimations.cs
+++ Assets/Scripts/HUDs & UI/UIAnimations.cs
@@ -5,4 +5,25 @@ using UnityEngine;
 public class UIAnimations : MonoBehaviour
 {
     [SerializeField] private GameObject topContainer, bottomContainer, examinationBox;
+    [SerializeField] private float time = 4f;
+
+    public void UnfoldToRight(GameObject gameO)
+    {
+        gameO.SetActive(true);
+        LeanTween.value(gameO, 0, gameO.GetComponent<RectTransform>().rect.width, time)
+            .setEaseOutExpo()
+            .setOnUpdate((value) => {
+                gameO.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, value);
+            });
+    }
+
+    private void Start()
+    {
+        topContainer.SetActive(false);
+        bottomContainer.SetActive(false);
+        examinationBox.SetActive(false);
+
+        UnfoldToRight(topContainer);
+        UnfoldToRight(bottomContainer);
+    }
 }
